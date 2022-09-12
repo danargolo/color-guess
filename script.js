c9de29fd4c9eed1d451889870bd3212c52deeb8e
Danilo Argolo <dan_argolo@hotmail.com>
2022-09-11 22:52:55 -0300

    Adiciona innerText ao p

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const balls = document.querySelectorAll('.ball');
+const text = document.querySelector('#rgb-color');
 
 function randomColor() {
   const r = Math.floor(Math.random() * 255);
@@ -8,12 +9,15 @@ function randomColor() {
   return `rgb(${r}, ${g}, ${b})`
 }
 
-//   
-//   return
-
+function randomBall() { 
+  return Math.floor(Math.random() * balls.length);
+}
 
 window.addEventListener('load', () => {
   for (let index = 0; index < balls.length; index += 1) {
     balls[index].style.backgroundColor = randomColor();    
   }
+  randomBall();
+  const findBall = balls[randomBall()];
+  text.innerText = `${findBall.style.backgroundColor}`;
 });
