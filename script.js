a9c10d9687b13672d43262a553a2903e8a6e08af
Danilo Argolo <dan_argolo@hotmail.com>
2022-09-12 00:49:23 -0300

    Resolvendo quesito 6 e 7

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const balls = document.querySelectorAll('.ball');
 const text = document.querySelector('#rgb-color');
+const chosenOne = document.getElementsByClassName('select');
+const result = document.getElementById('answer');
+const score = document.querySelector('#score');
 
 function randomColor() {
   const r = Math.floor(Math.random() * 255);
@@ -20,16 +23,14 @@ function loadgame() {
   }
   randomBall();
   text.innerText = `${guessBall.style.backgroundColor}`.replace('rgb', '');
+  result.innerText = 'Escolha uma cor.';
 }
-
 window.addEventListener('load', loadgame);
-
-const chosenOne = document.getElementsByClassName('select');
-const result = document.getElementById('answer');
 
 function answer() {
   if (chosenOne[0].style.backgroundColor === guessBall.style.backgroundColor) {
     result.innerText = 'Acertou!';
+    score.innerText = parseInt(score.innerText, 10) + 3;
   } else {
     result.innerText = 'Errou! Tente novamente!';
   }
@@ -46,3 +47,5 @@ function select(ref) {
 for (let index = 0; index < balls.length; index += 1) {
   balls[index].addEventListener('click', select);
 }
+
+document.getElementById('reset-game').addEventListener('click', loadgame);
